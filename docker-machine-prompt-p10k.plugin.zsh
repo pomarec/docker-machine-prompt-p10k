9dd56c3bc634799b19c7c29e008466bd3d0a9382
P.O Marec <po@marec.me>
2022-09-17 19:42:00 +0200

    Docker context support

--- docker-machine-prompt-p10k.plugin.zsh
+++ docker-machine-prompt-p10k.plugin.zsh
@@ -1,5 +1,10 @@
 function prompt_docker-machine() {
     if [[ $DOCKER_MACHINE_NAME != "" && $DOCKER_MACHINE_NAME != "default" ]]; then
-        p10k segment  -f 3  -i "🐳" -t " $DOCKER_MACHINE_NAME"
+        p10k segment -f 1 -i "" -t " $DOCKER_MACHINE_NAME"
+    else
+        DOCKER_CONTEXT_NAME=$(docker context inspect --format "{{ .Name }}")
+        if [[ $DOCKER_CONTEXT_NAME != "" && $DOCKER_CONTEXT_NAME != "default" ]]; then
+            p10k segment -f 4 -i "" -t " $DOCKER_CONTEXT_NAME"
+        fi
     fi
 }
