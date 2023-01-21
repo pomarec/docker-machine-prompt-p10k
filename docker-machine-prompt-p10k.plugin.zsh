56e98e350d2ab418ed34342517ba724b557d730b
P.O Marec <po@marec.me>
2023-01-21 18:20:31 +0100

    improve context read speed

--- docker-machine-prompt-p10k.plugin.zsh
+++ docker-machine-prompt-p10k.plugin.zsh
@@ -1,9 +1,13 @@
 function prompt_docker-machine() {
     if [[ $DOCKER_MACHINE_NAME != "" && $DOCKER_MACHINE_NAME != "default" ]]; then
-        p10k segment -f 1 -i "" -t " $DOCKER_MACHINE_NAME"
+        p10k segment -f 4 -i "" -t " $DOCKER_MACHINE_NAME"
     else
-        DOCKER_CONTEXT_NAME=$(docker context inspect --format "{{ .Name }}")
-        if [[ $DOCKER_CONTEXT_NAME != "" && $DOCKER_CONTEXT_NAME != "default" ]]; then
+        DOCKER_CONTEXT_NAME="null"
+        if (( $+commands[jq] )); then
+            local cfg=${DOCKER_CONFIG:-~/.docker/config.json}
+            DOCKER_CONTEXT_NAME=$(jq -e -r '.currentContext' $cfg 2>/dev/null)
+        fi
+        if [[ $DOCKER_CONTEXT_NAME != "" && $DOCKER_CONTEXT_NAME != "null" ]]; then
             p10k segment -f 4 -i "" -t " $DOCKER_CONTEXT_NAME"
         fi
     fi
